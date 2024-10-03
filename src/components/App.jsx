c4222cf2a9ec92ccae42a2f23421948c3291cf7f
Aaron Thamin <aaron.thamin@tum.de>
2024-10-03 12:14:04 +0200

    Add language functionality to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+// import React from "react";
 import Hero from "./Hero";
 import "./App.css";
+import Navbar from "./Navbar";
 
 const langEN = false;
 const langDE = false;
 
 function setLanguage() {
-  if (langEN) {
-    return "en";
-  } else if (langDE) {
-    return "de";
-  }
-  return "id";
+	if (langEN) {
+		return "en";
+	} else if (langDE) {
+		return "de";
+	}
+	return "id";
 }
 
-export default function App() {
-	return <Hero lang={setLanguage()}/>;
+function App() {
+	return (
+		<div>
+			<Navbar />
+			<Hero lang={setLanguage()} />
+		</div>
+	);
 }
+
+export default App;
