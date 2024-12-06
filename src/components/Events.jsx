4f08aaf6031d519275ad9e9818f0724c506c6798
Aaron Thamin <aaron.thamin@tum.de>
2024-12-06 20:27:43 +0100

    Added upcoming events card using google api

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 import Navbar from "./elements/Navbar";
 import Footer from "./elements/Footer";
+import CalendarEvents from "./UpcomingCalendarEvents";
 
 const Events = () => {
+	const { t } = useTranslation();
+
 	return (
 		<div>
 			<Navbar activeLink="events" />
+			<div className="">
+				<h1>{t("upcoming-events")}</h1>
+				<CalendarEvents />
+			</div>
 			<Footer />
 		</div>
 	);
