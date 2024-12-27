e8a5f8fea2aeb7d4619fafe0ae4d4bcf201e2832
Aaron Thamin <aaron.thamin@tum.de>
2024-12-27 11:17:50 +0100

    Added row placeholder for kolekte

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Navbar from "./elements/Navbar";
 import Hero from "./elements/Hero";
 import Section from "./elements/Section";
@@ -10,8 +11,27 @@ const Home = () => {
 		<div>
 			<Navbar activeLink="home" />
 			<Hero />
-			<Section title={"past-events"} alignment={"center"} />
-			<PastEvents />
+			<div className="content">
+				<Section title={"past-events"} alignment={"center"} />
+				<PastEvents />
+			</div>
+			<div className="content">
+				<Section title={"kolekte"} alignment={"center"} />
+				<Container>
+					<Row>
+						<Col>placeholder qr paypal</Col>
+						<Col>
+							<p>
+								“Peringatkanlah agar mereka itu berbuat baik, menjadi kaya dalam
+								kebajikan, suka memberi dan membagi dan dengan demikian
+								mengumpulkan suatu harta sebagai dasar yang baik bagi dirinya di
+								waktu yang akan datang untuk mencapai hidup yang sebenarnya.”
+							</p>
+							<p>1 Timotius 6:17-19</p>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 			<Footer />
 		</div>
 	);
