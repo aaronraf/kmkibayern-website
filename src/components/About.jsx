b4236dbcb8c3b4a5a0db731887cb9179c5997bfa
Aaron Thamin <aaron.thamin@tum.de>
2025-05-16 20:02:50 +0200

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Navbar from "./elements/Navbar";
+import Profile from "./elements/Profile";
 import Footer from "./elements/Footer";
-import fotoPK from "../assets/PK-24-25.png"
+import fotoPK from "../assets/pk-2425/pk-2425.png";
+import db from "../assets/database-pk.json";
 
 const About = () => {
 	const { t } = useTranslation();
@@ -16,6 +18,11 @@ const About = () => {
 					<img src={fotoPK} alt="Panitia Kerja 2024/2025" />
 					<p>Panitia Kerja KMKI Bayern Periode 2024/2025</p>
 				</div>
+				<div className="pk-sekarang">
+					<h2>Panitia Kerja 2024/2025</h2>
+					<Profile index={0} imgFolderPath="2425" />
+				</div>
+				<div className="pk-lama"></div>
 			</div>
 			<Footer />
 		</div>
