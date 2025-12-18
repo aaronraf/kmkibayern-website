65fdcdf2c60b9bf25d45dad95c114abb42111e0d
Aaron Thamin <aaron.thamin@tum.de>
2025-12-18 21:28:35 +0100

    Updated snowfall effects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import Snowfall from "react-snowfall";
 import Home from "./Home";
 import Events from "./Events";
 import About from "./About";
@@ -8,9 +9,12 @@ import "./App.css";
 import i18n from "../i18n";
 
 const App = () => {
+	const month = new Date().getMonth();
+
 	return (
 		<Router>
 			<div>
+				{(month == 11 || month <= 1) && <Snowfall />}
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/events" element={<Events />} />
